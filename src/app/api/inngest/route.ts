317fae2297f7aa69ac7f8b2f60280a45fa7f9953
bilalkhanx9 <bilalkhanx9@users.noreply.github.com>
2026-09-08 05:35:54 +0500

    chore: remove all old project backend actions, inngest, r2, seeds, and old documentation

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
-import { handleTaskAssigned, scheduleDueReminder } from "@/inngest/functions/task-reminders";
-import { sendWeeklyDigest } from "@/inngest/functions/weekly-digest";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [handleTaskAssigned, scheduleDueReminder, sendWeeklyDigest],
-});
